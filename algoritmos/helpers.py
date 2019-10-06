b8da9cc9e59ce6398fff1fe896d6b691718a18f9
Lucas Hiago <lucashiago63@gmail.com>
2019-10-06 17:53:43 -0300

    Remove commments from helpers

--- algoritmos/helpers.py
+++ algoritmos/helpers.py
@@ -1,7 +1,3 @@
-"""
-Esse arquivo vai conter algumas funções 
-úteis para todos os algoritmos
-"""
 from copy import deepcopy
 from .node import No
 import sys
@@ -16,7 +12,6 @@ def transforma_em_array(jogo):
     return novo
 
 def verifica_venceu(jogo):
-    #Jogo é um array bidimensional
     estado_objetivo = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
     for i in range(3):
@@ -26,12 +21,11 @@ def verifica_venceu(jogo):
     return True
 
 def get_posicao_vazio(jogo):
-    #Jogo é um array bidimensional
-    #Retorna a posição linha, coluna da posição vazia
-    espaço_vazio = 0
+    espaco_vazio = 0
+    espaco_vazio_txt = ' '
     for i in range(3):
         for j in range(3):
-            if jogo[i][j] == espaço_vazio:
+            if jogo[i][j] == espaco_vazio or jogo[i][j] == espaco_vazio_txt:
                 return i, j
 
 def mostra_array(jogo):
@@ -41,51 +35,38 @@ def mostra_array(jogo):
         print('')
 
 def gera_filhos(jogo):
-    #Jogo é um no
-    #Retorna uma lista com todos as jogadas possiveis
     i, j = get_posicao_vazio(jogo.jogo)
     possibilidades = []
     limite_superior = limite_esquerda = 0
     limite_inferior = limite_direita = 2
-    # mostra_array(jogo.jogo)
 
-    #Mover para a esquerda
     if j > limite_esquerda:
         novo = deepcopy(jogo.jogo)
         novo[i][j - 1], novo[i][j] = novo[i][j], novo[i][j - 1]
         novo = No(novo, 'Esquerda', jogo, jogo.custo + 1)
-        # mostra_array(novo.jogo)
         possibilidades.append(novo)
 
-    #Mover para a direita
     if j < limite_direita:
         novo = deepcopy(jogo.jogo)
         novo[i][j + 1], novo[i][j] = novo[i][j], novo[i][j + 1]
         novo = No(novo, 'Direita', jogo, jogo.custo + 1)
-        # mostra_array(novo.jogo)
         possibilidades.append(novo)
-
-    #Mover para cima
+    
     if i > limite_superior:
         novo = deepcopy(jogo.jogo)
         novo[i - 1][j], novo[i][j] = novo[i][j], novo[i - 1][j]
         novo = No(novo, 'Cima', jogo, jogo.custo + 1)
-        # mostra_array(novo.jogo)
         possibilidades.append(novo)
 
-    #Mover para baixo
     if i < limite_direita: 
         novo = deepcopy(jogo.jogo)
         novo[i + 1][j], novo[i][j] = novo[i][j], novo[i + 1][j]
         novo = No(novo, 'Baixo', jogo, jogo.custo + 1)
-        # mostra_array(novo.jogo)
         possibilidades.append(novo)
     
     return possibilidades
 
 def get_caminho(no):
-    #Recebe o no da solução
-    #Faz todo o caminho inverso pegando os passos
     caminho_das_pedras = []
     atual = no
     while atual.pai != None:
@@ -95,21 +76,16 @@ def get_caminho(no):
     return caminho_das_pedras
 
 def get_custo(no):
-    #Recebe o nó da solução
     custo = len(get_caminho_das_pedras(no))
     return custo
 
 def get_profundidade_maxima(no, profundidade=0):
-    #Recebe um no de teste
-    #Vê se o custo dele foi o maior
     custo = get_custo(no)
     if custo > profundidade:
         profundidade = custo
     return profundidade
 
 def get_caminho_das_pedras(no):
-    #Recebe o nó vitorioso
-    #Retorna o array com o caminho completo
     caminho = []
     atual = no
     while atual.pai != None:
@@ -125,12 +101,11 @@ def prepara_dic(no, algoritmo, tempo, gerados, maxima, ram):
         'profundidade_solucao': "Profundidade da Solução: " + str(get_custo(no)),
         'maxima_profundidade': "Profundiade máxima atingida: " + str(maxima),
         'acoes': get_caminho_das_pedras(no),
-        'ram': "Uso de RAM: " + str(round(ram), 2) + " MB"
+        'ram': "Uso de RAM: " + str(round(ram, 2)) + " MB"
     }
     return dic
 
 def get_ram():
-
     if sys.platform == "win32":
         import psutil
         return psutil.Process().memory_info().rss/1024/1024
