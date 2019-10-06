ac1aa232d37bfa7bb6ad49145a644c5e8f0238a8
Lucas Hiago <lucashiago63@gmail.com>
2019-10-06 17:27:15 -0300

    Add GUI for user utilization

---
+++ resultados.py
@@ -0,0 +1,100 @@
+from tkinter import *
+from algoritmos import helpers
+
+class Resultado:
+    def __init__(self, raiz, resultado, inicial):
+        self.raiz = raiz
+        self.raiz.title('Resultado')
+
+        self.cont9 = Frame(raiz)
+        self.cont9.pack(side=BOTTOM)
+
+        self.cont0 = Frame(raiz)
+        self.cont0.pack(side=RIGHT)
+
+        group = LabelFrame(raiz, text="Tabuleiro", padx=5, pady=5)
+        group.pack(padx=20, pady=10)
+        self.passos = list(resultado['acoes'])
+        self.matriz = inicial
+
+        self.cont1 = Frame(group)
+        self.cont1.pack()
+        self.m1 = Entry(self.cont1)
+        self.m1['width'] = 5
+        self.m1.insert(0, str(self.matriz[0][0]))
+        self.m1.pack(side=LEFT)
+        self.m2 = Entry(self.cont1)
+        self.m2['width'] = 5
+        self.m2.insert(0, str(self.matriz[0][1]))
+        self.m2.pack(side=LEFT)
+        self.m3 = Entry(self.cont1)
+        self.m3['width'] = 5
+        self.m3.insert(0, str(self.matriz[0][2]))
+        self.m3.pack(side=LEFT)
+
+        self.cont2 = Frame(group)
+        self.cont2.pack()
+        self.m4 = Entry(self.cont2)
+        self.m4['width'] = 5
+        self.m4.insert(0, str(self.matriz[1][0]))
+        self.m4.pack(side=LEFT)
+        self.m5 = Entry(self.cont2)
+        self.m5['width'] = 5
+        self.m5.insert(0, str(self.matriz[1][1]))
+        self.m5.pack(side=LEFT)
+        self.m6 = Entry(self.cont2)
+        self.m6['width'] = 5
+        self.m6.insert(0, str(self.matriz[1][2]))
+        self.m6.pack(side=LEFT)
+
+        self.cont3 = Frame(group)
+        self.cont3.pack()
+        self.m7 = Entry(self.cont3)
+        self.m7['width'] = 5
+        self.m7.insert(0, str(self.matriz[2][0]))
+        self.m7.pack(side=LEFT)
+        self.m8 = Entry(self.cont3)
+        self.m8['width'] = 5
+        self.m8.insert(0, str(self.matriz[2][1]))
+        self.m8.pack(side=LEFT)
+        self.m9 = Entry(self.cont3)
+        self.m9['width'] = 5
+        self.m9.insert(0, str(self.matriz[2][2]))
+        self.m9.pack(side=LEFT)
+
+        group2 = LabelFrame(self.cont0, text="Solução", padx=5, pady=5)
+        group2.pack(padx=20, pady=10)
+        Label(group2, text=resultado['algoritmo']).pack()
+        Label(group2, text=resultado['tempo']).pack()
+        Label(group2, text=resultado['gerados']).pack()
+        Label(group2, text=resultado['profundidade_solucao']).pack()
+        Label(group2, text=resultado['maxima_profundidade']).pack()
+        Label(group2, text=resultado['ram']).pack()
+        self.prog = Button(self.cont9, text='Próximo passo',
+                           padx=2, command=self.movimento)
+        self.prog.pack(pady=10, padx=5, side=RIGHT)
+
+    def movimento(self):
+        if len(self.passos) == 0:
+            return None
+        mov = self.passos[0]
+        del self.passos[0]
+        self.matriz = helpers.proximo(self.matriz, mov)
+        self.m1.delete(0, END)
+        self.m1.insert(0, str(self.matriz[0][0]))
+        self.m2.delete(0, END)
+        self.m2.insert(0, str(self.matriz[0][1]))
+        self.m3.delete(0, END)
+        self.m3.insert(0, str(self.matriz[0][2]))
+        self.m4.delete(0, END)
+        self.m4.insert(0, str(self.matriz[1][0]))
+        self.m5.delete(0, END)
+        self.m5.insert(0, str(self.matriz[1][1]))
+        self.m6.delete(0, END)
+        self.m6.insert(0, str(self.matriz[1][2]))
+        self.m7.delete(0, END)
+        self.m7.insert(0, str(self.matriz[2][0]))
+        self.m8.delete(0, END)
+        self.m8.insert(0, str(self.matriz[2][1]))
+        self.m9.delete(0, END)
+        self.m9.insert(0, str(self.matriz[2][2]))
---
+++ solucao.py
@@ -0,0 +1,118 @@
+from algoritmos import helpers, node, profundidade, largura, gulosa, a_estrela
+from tkinter import *
+from tkinter import messagebox
+from resultados import Resultado
+
+class Jogo:
+    def __init__(self, master):
+        self.frame1 = Frame(master)
+        self.frame1.pack(side=TOP, padx=10, pady=10)
+        self.matriz = LabelFrame(master, padx=20, pady=20)
+        self.matriz.pack()
+        self.cont1 = Frame(self.matriz)
+        self.cont1.pack()
+        self.e1 = Entry(self.cont1, width=5)
+        self.e1.pack(side=LEFT)
+        self.e2 = Entry(self.cont1, width=5)
+        self.e2.pack(side=LEFT)
+        self.e3 = Entry(self.cont1, width=5)
+        self.e3.pack(side=LEFT)
+        self.cont2 = Frame(self.matriz)
+        self.cont2.pack()
+        self.e4 = Entry(self.cont2, width=5)
+        self.e4.pack(side=LEFT)
+        self.e5 = Entry(self.cont2, width=5)
+        self.e5.pack(side=LEFT)
+        self.e6 = Entry(self.cont2, width=5)
+        self.e6.pack(side=LEFT)
+        self.cont3 = Frame(self.matriz)
+        self.cont3.pack()
+        self.e7 = Entry(self.cont3, width=5)
+        self.e7.pack(side=LEFT)
+        self.e8 = Entry(self.cont3, width=5)
+        self.e8.pack(side=LEFT)
+        self.e9 = Entry(self.cont3, width=5)
+        self.e9.pack(side=LEFT)
+        algo1 = Button(self.matriz, text="Busca em Largura", command=self.largura)
+        algo2 = Button(self.matriz, text="Busca em Profundidade", command=self.profundidade)
+        algo3 = Button(self.matriz, text="Busca Gulosa", command=self.gulosa)
+        algo4 = Button(self.matriz, text="Busca A*", command=self.a_estrela)
+        algo1.pack(side=TOP)
+        algo2.pack(side=TOP)
+        algo3.pack(side=BOTTOM)
+        algo4.pack(side=BOTTOM)
+
+    def get_tabuleiro(self):
+        tabuleiro = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        try:
+            tabuleiro[0][0] = int(self.e1.get().strip())
+        except:
+            tabuleiro[0][0] = 0
+        try:
+            tabuleiro[0][1] = int(self.e2.get().strip())
+        except:
+            tabuleiro[0][1] = 0
+        try:
+            tabuleiro[0][2] = int(self.e3.get().strip())
+        except:
+            tabuleiro[0][2] = 0
+        try:
+            tabuleiro[1][0] = int(self.e4.get().strip())
+        except:
+            tabuleiro[1][0] = 0
+        try:
+            tabuleiro[1][1] = int(self.e5.get().strip())
+        except:
+            tabuleiro[1][1] = 0
+        try:
+            tabuleiro[1][2] = int(self.e6.get().strip())
+        except:
+            tabuleiro[1][2] = 0
+        try:
+            tabuleiro[2][0] = int(self.e7.get().strip())
+        except:
+            tabuleiro[2][0] = 0
+        try:
+            tabuleiro[2][1] = int(self.e8.get().strip())
+        except:
+            tabuleiro[2][1] = 0
+        try:
+            tabuleiro[2][2] = int(self.e9.get().strip())
+        except:
+            tabuleiro[2][2] = 0
+        return tabuleiro
+    
+    def largura(self):
+        tabuleiro = self.get_tabuleiro()
+        jogo = node.No(tabuleiro, "Inicial")
+        self.mostra_resultado(largura.busca(jogo), tabuleiro)
+
+    def profundidade(self):
+        tabuleiro = self.get_tabuleiro()
+        jogo = node.No(tabuleiro, "Inicial")
+        self.mostra_resultado(profundidade.busca(jogo), tabuleiro)
+
+    def gulosa(self):
+        tabuleiro = self.get_tabuleiro()
+        jogo = node.No(tabuleiro, "Inicial")
+        self.mostra_resultado(gulosa.busca(jogo), tabuleiro)
+
+    def a_estrela(self):
+        tabuleiro = self.get_tabuleiro()
+        jogo = node.No(tabuleiro, "Inicial")
+        self.mostra_resultado(a_estrela.busca(jogo), tabuleiro)
+    
+    def mostra_resultado(self, resultado, inicial):
+        if not resultado:
+            messagebox.showinfo("\ Erro!", "Não foi encontrada solução")
+        else:
+            nova = Tk()
+            Resultado(nova, resultado, inicial)
+            nova.mainloop()
+
+root = Tk()
+Jogo(root)
+root.title("8 Puzzle")
+root.geometry("400x400")
+root.mainloop()
+
